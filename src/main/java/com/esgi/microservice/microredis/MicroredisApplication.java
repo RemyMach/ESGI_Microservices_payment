b923d642415fdff07e654bae96f91d857af8109b
Rémy Machavoine <remygt@hotmail.fr>
2022-05-18 08:45:12 +0200

    init repository

---
+++ src/main/java/com/esgi/microservice/microredis/MicroredisApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.microservice.microredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroredisApplication.class, args);
+	}
+
+}
