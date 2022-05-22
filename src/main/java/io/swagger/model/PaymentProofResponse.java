0309e3c4338859719a9d9cd9296c96efc9084feb
Rémy Machavoine <remygt@hotmail.fr>
2022-05-22 22:07:37 +0200

    fix(redisPayment): Add redis response

--- src/main/java/io/swagger/model/PaymentProofResponse.java
+++ src/main/java/io/swagger/model/PaymentProofResponse.java
@@ -1,7 +1,12 @@
 package io.swagger.model;
 
-public record PaymentProofResponse(
-        String transactionId,
-        String date
-) {
+public class PaymentProofResponse{
+    public String transactionId;
+    public String date;
+
+    public PaymentProofResponse(String transactionId, String date) {
+        this.transactionId = transactionId;
+        this.date = date;
+    }
+
 }
